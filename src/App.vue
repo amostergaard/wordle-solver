11683b8d3679224a95fdbb89a120652382050589
Amelia Ostergaard <amelia.marie.ostergaard@gmail.com>
2022-10-14 20:08:38 -0600

    test naive-ui functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import { NButton } from "naive-ui";
+</script>
+
 <template>
   <div>
-    <h1>Hello, world!</h1>
+    <h1>Try to get Naive UI working</h1>
+    <p>Test button</p>
+    <n-button>Press me</n-button>
   </div>
 </template>
