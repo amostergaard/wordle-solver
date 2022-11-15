7e5d4a06200703b1d902dc6b188319508c37d9e7
Amelia Ostergaard <amelia.marie.ostergaard@gmail.com>
2022-11-14 21:04:49 -0700

    content: Test that @headlessui/vue is working correctly

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Switch } from "@headlessui/vue";
+
+const enabled = ref(false);
+</script>
+
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <div class="bg-black rounded-lg text-white">Box</div>
+
+    <hr />
+
+    <h2>Toggle Test</h2>
+
+    <div class="py-16">
+      <Switch
+        v-model="enabled"
+        :class="enabled ? 'bg-teal-900' : 'bg-teal-700'"
+        class="relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        <span class="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          :class="enabled ? 'translate-x-9' : 'translate-x-0'"
+          class="pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+        />
+      </Switch>
+    </div>
+
+    <p>Toggle is {{ enabled ? "enabled" : "disabled" }}</p>
   </div>
 </template>
